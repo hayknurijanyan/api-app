3c53be9153ea19f0b850cd913b07a782861cdeb9
Hayk <58630378+hayknurijanyan@users.noreply.github.com>
2020-07-09 04:32:42 +0400

    random quotes

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
-const button = document.getElementById('button')
-button.innerHTML = 'Press'
-button.addEventListener('click',addPhoto)
-const img = document.getElementById('img')
-console.log(img)
 
-async function addPhoto() {
-    const response = await fetch ('https://dog.ceo/api/breeds/image/random')
-    let data = await response.json()
-    img.src=data.message
+const button = document.getElementById('button');
+const quote = document.getElementById('quote');
+const author = document.getElementById('author');
+button.innerHTML = 'Press';
+author.innerHTML = '';
+quote.innerHTML = 'Press to start :)';
+button.addEventListener('click',randomQuote)
+
+
+async function randomQuote() {
+    const response = await fetch ('https://thesimpsonsquoteapi.glitch.me/quotes')
+    let data = await response.json();
+    let m =  [...data][0];
+    author.innerHTML = m.character;
+    quote.innerHTML = `"${m.quote}"`;
+    
 }
